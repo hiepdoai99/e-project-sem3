a92c66d8659f52aac76ff26ded75218fead7a705
hiepdoai99 <nguyenxuanhiepk49@gmail.com>
2024-05-08 19:42:30 +0700

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,9 @@
+import Home from "../views/Home.vue";
+
+export const routes = [
+    {
+      path: "/",
+      component: Home,
+      meta: {title: 'Admin'}
+    },
+];
---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<script setup>
+import LayoutAdmin from "../layouts/LayoutAdmin.vue";
+</script>
+
+<template>
+<LayoutAdmin>
+<div>
+
+</div>
+</LayoutAdmin>
+</template>
